8f5b74980311ee10fb08fc4d64e56edd1cbb67fd
Anatoly Emelyanov <aemelyanov7@gmail.com>
2023-02-03 14:14:45 +0300

    Задача без четных чисел и среднего значения

---
+++ src/HOMEWORK3.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class HOMEWORK3 {
+
+    public static void main(String[] args){
+        int[] array = new int[]{10, 34, 3, 2, 32, 12};
+
+
+        int max = getMax(array);
+        System.out.println("Maximum Value is: "+max);
+
+
+        int min = getMin(array);
+        System.out.println("Minimum Value is: "+min);
+
+    }
+    public List<Integer> removeEvenValue(List<Integer> list){
+        
+
+        return new ArrayList<>();
+    }
+    public Integer getAverage(List<Integer> list){
+
+        return 0;
+    }
+
+
+    public static int getMax(int[] inputArray){
+        int maxValue = inputArray[0];
+        for(int i=1;i < inputArray.length;i++){ if(inputArray[i] > maxValue){
+            maxValue = inputArray[i];
+        }
+        }
+        return maxValue;
+    }
+
+
+    public static int getMin(int[] inputArray){
+        int minValue = inputArray[0];
+        for(int i=1;i<inputArray.length;i++){
+            if(inputArray[i] < minValue){
+                minValue = inputArray[i];
+            }
+        }
+        return minValue;
+    }
+
+
+}
+
+
+
